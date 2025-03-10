2cc6dafe2e5257ef2e938c053e11fd4820c928a5
Shrinivas <shrinivaskshreeni@gmail.com>
2025-03-10 15:43:13 +0000

    Updated hello.cpp with new print statement

--- hello.cpp
+++ hello.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
+  cout << "Hello, shrinivas how are you daa\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
